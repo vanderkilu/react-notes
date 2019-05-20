e0d7af0d3bfe5993ebfd6557775fbcc62769f81f
vanderkilu <vanderkiliku@gmail.com>
2019-05-20 13:51:20 -0400

    added state to todolist component

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Todo.css'
-function Todo() {
+function Todo(props) {
     return (
         <div className="todo">
             <p className="todo__done-btn"></p>
-            <p className="todo__task">just creating a todo</p>
+            <p className="todo__task">{props.task.task}</p>
             <p className="todo__delete-icon">&times;</p>
         </div>
     )
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import Todo from '../Todo/Todo.js'
+import './TodoList.css'
 
 class TodoList extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            todos: [
+                {task: "going parting", id:0}, 
+                {task: "watching hobbit", id:1},
+                {task: "read at least two pages on pragmatic programmer",id:2, }
+            ]
+        }
     }
     render() {
+        const tasks = this.state.todos.map(todo => 
+            <Todo task={todo} key={todo.id}/>
+        )
         return (
             <div className="todoWrapper">
-                <Todo/>
+                {tasks}
             </div>
         )
     }

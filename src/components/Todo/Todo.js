863b1dc6c9536c6710092e76ac2c178bb8908f0b
vanderkilu <vanderkiliku@gmail.com>
2019-05-20 20:04:49 -0400

    toggle between completed anduncompleted icon

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import './Todo.css'
 function Todo(props) {
+    let isDoneElem
+    if (!props.todo.completed) {
+        isDoneElem = <p className="todo__done-icon"></p>
+    }
+    else {
+        isDoneElem = <i className="fa fa-check todo__check-icon"></i>
+    }
+    
     return (
-        <div className="todo">
-            <p className={props.todo.completed ? "todo__done-btn done": "todo__done-btn"}
-                onClick={props.onClick}>
-            </p>
+        <div className={props.todo.completed ? " todo todo-complete" : "todo" } 
+            onClick={props.onClick}>
+            {isDoneElem}
             <p className={props.todo.completed ? "todo__task completed ":"todo__task"}>{props.todo.task}</p>
             <i className="todo__delete-icon fa fa-times"></i>
         </div>

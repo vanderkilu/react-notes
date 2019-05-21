dd2d1888fab27c5aab95f8ebfce6050e9830ff3e
vanderkilu <vanderkiliku@gmail.com>
2019-05-21 11:21:46 -0400

    add theme switch functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import TodoList from './components/TodoList/TodoList'
 
 function App() {
+  const handleClick = () => {
+    const html = document.getElementsByTagName('html')[0]
+    html.classList.toggle('light-theme')
+  }
   return (
     <div className="wrapper">
       <h3 className="app-name">TodoLy</h3>
@@ -12,6 +16,19 @@ function App() {
           </span>
       </p>
        <TodoList/>
+       <div className="footer">
+          <p className="footer__by">
+            Made With  
+            <span role="img" className="emoji" aria-label="love-emoji">
+                💜
+            </span>
+            love by <a href="http://twitter.com/kweku_kilu" 
+                    target="_blank" rel="noopener noreferrer"> kweku_kilu</a>
+          </p>
+          <div className="footer__theme">
+              <span className="theme" onClick={ ()=> handleClick() }></span>
+          </div>
+       </div>
     </div>
   );
 }
